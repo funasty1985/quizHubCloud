fd01b93956dd5c15f6f266961d94db094e84507e
Jungyu Lee <113372052+jlee629@users.noreply.github.com>
2024-03-21 19:56:30 -0400

    Updated app.py

--- Capabilities/app.py
+++ Capabilities/app.py
@@ -1,29 +1,22 @@
 from chalice import Chalice
+from chalicelib import comprehend_service
 
 app = Chalice(app_name='Capabilities')
 
+comprehend_service = comprehend_service.ComprehendService()
 
-@app.route('/')
+@app.route('/', cors=True)
 def index():
     return {'hello': 'world'}
 
+@app.route('/extract-key-phrases', methods=['POST'], cors=True)
+def extract_key_phrases():
+    request = app.current_request
+    body = request.json_body
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user'on}
-#
-# See the README documentation for more examples.
-#
+    text = body.get('text', '')
+    if not text:
+        return {'error': 'No text provided'}
+
+    key_phrases = comprehend_service.extract_key_phrases(text)
+    return {'keyPhrases': key_phrases}
