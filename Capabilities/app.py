c9473b312166544589495148f3d61ca7d1fa2425
Won Lee <jlee629@my.centennicalcollege.ca>
2024-03-29 17:35:02 -0400

    paragraph detection part added

--- Capabilities/app.py
+++ Capabilities/app.py
@@ -33,10 +33,10 @@ def upload():
         file_bytes = base64.b64decode(request_data['filebytes'])
 
         app.log.debug(f"Attempting to upload {file_name} to S3.")
-        pdf_info = storage_service.upload_file(file_bytes, file_name)
-        app.log.debug(f"Upload successful: {pdf_info}")
+        file_info = storage_service.upload_file(file_bytes, file_name)
+        app.log.debug(f"Upload successful: {file_info}")
 
-        return pdf_info
+        return file_info
     except Exception as e:
         app.log.error(f"Error processing or uploading PDF file: {str(e)}")
         return Response(body={'message': f'Error processing or uploading PDF file: {str(e)}'}, status_code=500)
@@ -59,3 +59,20 @@ def extract_text():
     except Exception as e:
         app.log.error(f"Error extracting text from the file: {str(e)}")
         return Response(body={'message': f'Error extracting text from the file: {str(e)}'}, status_code=500)
+
+@app.route('/extract-paragraph', methods=['POST'], cors=True)
+def extract_paragraph():
+    try:
+        request_data = json.loads(app.current_request.raw_body.decode('utf-8'))
+        file_name = request_data['filename']
+
+        extracted_text = textract_service.extract_paragraph(storage_service.get_storage_location(), file_name)
+
+        app.log.debug("Text extraction completed.")
+        app.log.debug(f"Extracted text: {extracted_text}")
+
+        return {'filename': file_name, 'extractedText': extracted_text}
+
+    except Exception as e:
+        app.log.error(f"Error extracting text from PDF: {str(e)}")
+        return Response(body={'message': f'Error extracting text from PDF: {str(e)}'}, status_code=500)
--- Capabilities/chalicelib/textract_service.py
+++ Capabilities/chalicelib/textract_service.py
@@ -1,5 +1,7 @@
 import boto3
 import time
+
+
 class TextractService:
     def __init__(self):
         self.client = boto3.client('textract')
@@ -30,38 +32,47 @@ class TextractService:
                     text += item['Text'] + "\n"
         return text
 
-    # def extract_text(self, bucket_name, document_name):
-    #     response = self.client.start_document_text_detection(
-    #         DocumentLocation={
-    #             'S3Object': {
-    #                 'Bucket': bucket_name,
-    #                 'Name': document_name
-    #             }
-    #         }
-    #     )
-    #     job_id = response['JobId']
-    #     print(f"Started job with id: {job_id}")
+    def extract_paragraph(self, bucket_name, document_name):
+        response = self.client.start_document_text_detection(
+            DocumentLocation={
+                'S3Object': {
+                    'Bucket': bucket_name,
+                    'Name': document_name
+                }
+            }
+        )
+        job_id = response['JobId']
+        print(f"Started job with id: {job_id}")
 
-    #     status = ""
-    #     while status not in ["SUCCEEDED", "FAILED"]:
-    #         time.sleep(5)
-    #         response = self.client.get_document_text_detection(JobId=job_id)
-    #         status = response['JobStatus']
+        status = ""
+        while status not in ["SUCCEEDED", "FAILED"]:
+            time.sleep(5)
+            response = self.client.get_document_text_detection(JobId=job_id)
+            status = response['JobStatus']
 
-    #     paragraphs = []
-    #     current_paragraph = ""
-    #     last_top = None
-    #     for item in response.get('Blocks', []):
-    #         if item['BlockType'] == "LINE":
-    #             current_top = item['Geometry']['BoundingBox']['Top']
-    #             if last_top is not None and (current_top - last_top) > 0.02:
-    #                 paragraphs.append(current_paragraph.strip())
-    #                 current_paragraph = item['Text'] + " "
-    #             else:
-    #                 current_paragraph += item['Text'] + " "
-    #             last_top = current_top
+        """
+        different file types have different thresholds for paragraph detection
+        """
+        file_extension = '.' + document_name.split('.')[-1].lower()
+        if file_extension == '.pdf':
+            threshold = 0.02
+        else:
+            threshold = 0.05
 
-    #     if current_paragraph:
-    #         paragraphs.append(current_paragraph.strip())
+        paragraphs = []
+        current_paragraph = ""
+        last_top = None
+        for item in response.get('Blocks', []):
+            if item['BlockType'] == "LINE":
+                current_top = item['Geometry']['BoundingBox']['Top']
+                if last_top is not None and (current_top - last_top) > threshold:
+                    paragraphs.append(current_paragraph.strip())
+                    current_paragraph = item['Text'] + " "
+                else:
+                    current_paragraph += item['Text'] + " "
+                last_top = current_top
 
-    #     return "\n\n".join(paragraphs)
+        if current_paragraph:
+            paragraphs.append(current_paragraph.strip())
+
+        return paragraphs
